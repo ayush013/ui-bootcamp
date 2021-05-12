7279f9d63733b3589a4986eb58b655402db5af59
Ayush Singh <ayush.singh.xda@gmail.com>
2021-05-12 12:21:04 +0530

    Init calendar from scaffold

---
+++ calendar/webpack.dev.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Calendar - Development',
+      template: 'index.html'
+    }),
+  ],
+};
