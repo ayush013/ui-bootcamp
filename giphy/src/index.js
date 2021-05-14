156769b292325f57ac7517d7962884b4a6feba66
Ayush Singh <ayush.singh.xda@gmail.com>
2021-05-14 13:36:34 +0530

    rendered search results via GET

--- giphy/src/index.js
+++ giphy/src/index.js
@@ -1,13 +1,50 @@
+import HttpClient from './app/http-client';
+import Store from './app/store';
 import View from './app/view';
 import './style.scss'
 
 export const loadApp = () => {
 
     const viewService = new View();
+    const httpService = new HttpClient();
+    const storeService = new Store();
 
-    viewService.initSearchListener((e) => {
-        console.log(e)
-    })
+    let searchTerm = '';
+
+    viewService.initSearchListener(async e => {
+        if (searchTerm) {
+            viewService.clearView();
+            storeService.clearStore();
+        }
+
+        searchTerm = e.target.value?.trim();
+
+        if (searchTerm) {
+            try {
+                let results = await httpService.getSearchResults(searchTerm);
+
+                results = storeService.transformData(results.data);
+
+                storeService.setStore(results);
+
+                results.forEach(result => {
+                    viewService.renderResult(result);
+                });
+
+            } catch (e) {
+                console.error(e);
+                alert('Failed to fetch results');
+                viewService.clearView();
+                viewService.clearSearch();
+                storeService.clearStore();
+            }
+        } else {
+            viewService.clearView();
+            storeService.clearStore();
+        }
+
+    });
+
 }
 
 loadApp();
